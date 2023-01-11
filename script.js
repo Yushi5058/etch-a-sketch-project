def8a49e6ed24d0bb9a9cd928a4c2865fb4c07db
chakroun-elias <eliaschakroun.01@gmail.com>
2023-01-11 14:03:54 +0100

    create script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const container = document.getElementById('container');
 const inputEl = document.getElementById('input-value');
 const input = inputEl.value;
+const size = input * input;
+
+document.getElementById('generate-grid-btn').addEventListener('click',createGrid);
+
+function createGrid() {
+    if (input >=1 && input <= 64) {
+        container.innerHTML = '';
+        generateGrid();
+    } else {
+        alert('Invalid input! Please select a number between 1 and 64.');
+    }
+}
 
 function generateGrid () {
-    for (i = 1; i < input; i++) {
-        if (input >=1 && input <= 64) {
-            let div = container.createElement('div');
+    for ( let i = 0; i < size; i++) {
+            let div = document.createElement('div');
             div.classList.add('square');
-            div.style.height = input + 'px';
-            div.style.width = input + 'px';
+            div.style.height = (1000 / input) + 'px';
+            div.style.width = ( 1000 / input) + 'px';
+            div.addEventListener('mouseenter', function() {
+                div.style.backgroundColor = 'black';
+            });
+            div.addEventListener('mouseleave', function() {
+                div.style.backgroundColor = 'white';
+            })
             container.appendChild(div);
-        } else {
-            alert('Invalid input!');
-        }
     } 
     }
